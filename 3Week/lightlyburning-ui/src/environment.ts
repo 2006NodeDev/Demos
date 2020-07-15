6e9d731bb18bfe17075d1cd0025a45779057d2e3
Abatson <abatson94@gmail.com>
2020-07-14 20:22:06 -0400

    env for axios

---
+++ 3Week/lightlyburning-ui/src/environment.ts
@@ -0,0 +1,11 @@
+
+export let lbBaseUrl:string
+//this is the only env we get in front end, and technically anyone that downloads the site can read it 
+if(process.env['NODE_ENV'] === 'production'){
+    //if we ran npm run build
+    //use the deployed address
+    lbBaseUrl = 'http://34.98.120.200'
+}else {
+    //we are in test or dev, use the local address
+    lbBaseUrl = 'http://localhost:2006'
+}
--- 3Week/lightlyburning-ui/src/remote/lightlyburning-api/index.ts
+++ 3Week/lightlyburning-ui/src/remote/lightlyburning-api/index.ts
@@ -1,10 +1,11 @@
 //this index is going to be for setting up the base axios client
 import axios from 'axios'
+import { lbBaseUrl } from '../../environment'
 
 
 // we will use this object to send off all of the other request we make to the lightlyburning api
 export const lightlyburningClient = axios.create({
-    baseURL:'http://localhost:2006',
+    baseURL:lbBaseUrl,
     headers:{
         'Content-Type': 'application/json'
     },
