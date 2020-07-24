c5725deb4f29c7e7bbd0f14d847070a63dbc3205
Abatson <abatson94@gmail.com>
2020-07-23 22:11:17 -0400

    changed password type

--- 6Week/user-service/src/daos/SQL/index.ts
+++ 6Week/user-service/src/daos/SQL/index.ts
@@ -7,7 +7,7 @@ import { Pool } from 'pg'
 export const connectionPool:Pool = new Pool({
     host: process.env['LB_HOST'],// the public ip address of you sql instance
     user: process.env['LB_USER'],//user on your database ( probably postgres)
-    password: process.env['LB_PASSWORD'],//password
+    password: Buffer.from(process.env['LB_PASSWORD'], 'base64').toString(),//password
     database: process.env['LB_DATABASE'],//name of database
     port:5432,// the database's port
     max:5//maximum number of connections
